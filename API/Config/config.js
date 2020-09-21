ccf4c3fb6159de45bcf36bf2aa69b860dede6cee
balaso_maske <balaso.maske@techbulls.com>
2020-09-21 11:43:00 +0530

    config url change

--- API/Config/config.js
+++ API/Config/config.js
@@ -1,7 +1,7 @@
 module.exports.defaultConfig = {
-    "connectionStringNEW": "mongodb+srv://balaso:balaso123@cluster0.yhqig.mongodb.net/balaso?retryWrites=true&w=majority",
+    "connectionString": "mongodb+srv://balaso:balaso123@cluster0.yhqig.mongodb.net/balaso?retryWrites=true&w=majority",
     "serverPort": 4000,
-    "connectionString": "mongodb://localhost/sample",
+    "connectionStringNEW": "mongodb://localhost/sample",
     "jwt":{
         secret: "5QdYLbXsONJJDkoEyQPk3aR7vng721veBww9pPglW1tRzp4nnUz3f4AewUPZl9YyyK590vdErx2KBfTu66GOrUOZs6CO9XzMAmQ8hddNqA7oVuV0cUuvEl7Hskqbofgr",
         expiresInMinutes: 2
